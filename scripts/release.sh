5c41e4ff270753bc3b9c9b7877acbffa3a1f0548
ShadiestGoat <48590492+ShadiestGoat@users.noreply.github.com>
2025-04-17 00:46:48 +0100

    script fix

--- scripts/release.sh
+++ scripts/release.sh
@@ -9,7 +9,9 @@ min_app=$(yq '.minAppVersion' "$base"/../manifest.json -oy)
 
 yq ".\"$new_version\" = \"$min_app\"" "$base"/../versions.json -ioj
 
-OUTPUT="$base/main.js" PROJECT_DIR="$base/.." node "$base"/esbuild.config.mjs production
+dist_dir="$base/../dist"
+
+OUTPUT="$dist_dir" PROJECT_DIR="$(realpath -e "$base/..")" node "$base"/esbuild.config.mjs production
 
 git -C "$base/.." add './package.json'
 git -C "$base/.." add './manifest.json'
@@ -20,6 +22,6 @@ git -C "$base/.." tag "$new_version" origin
 git -C "$base/.." push --tags
 
 gh release create "$new_version" --fail-on-no-commits --verify-tag --title "$new_version" --notes "" \
-	"$base"/main.js \
 	"$base"/../manifest.json \
-	"$base"/../styles.css
+	"$dist_dir"/main.js \
+	"$dist_dir"/styles.css
